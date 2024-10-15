1cea3756eeda9d2a621c00104a407a522ba2e24b
yevdias169 <yevindias@protonmail.com>
2024-10-15 19:56:31 +1100

    Cleaned file and uploaded changed to csv file

--- Cleaning.py
+++ Cleaning.py
@@ -2,3 +2,11 @@ import pandas as pd
 
 df = pd.read_csv("Medical_insurance.csv")
 
+df_cleaned = df.drop_duplicates()
+
+# Save the cleaned DataFrame back to the CSV file
+df_cleaned.to_csv('Medical_insurance.csv', index=False)
+
+
+
+
