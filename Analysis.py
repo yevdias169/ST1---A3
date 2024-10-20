099930db8c6ed4ec904c5ffb26fb8b5175546e4c
yevdias169 <yevindias@protonmail.com>
2024-10-20 16:58:27 +1100

    Code for categorical data analysis

--- Analysis.py
+++ Analysis.py
@@ -2,7 +2,14 @@ import pandas as pd
 
 df = pd.read_csv("Medical_insurance.csv")
 
-'''print(df.shape)
+#Basic information about the dataset and its numerical columns
+print(df.shape)
 print(df.size)
-print(df.info())'''
+print(df.info())
 print(df.describe())
+
+#Produce value counts for the catergorical data
+for col in df.select_dtypes(include=['object']).columns:
+    print(f"Value counts for {col}:")
+    print(df[col].value_counts())
+    print("\n")   
